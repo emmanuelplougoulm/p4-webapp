40cbfc12b99e3bc3fadfc1d065dcd660588feb50
e.plougoulm <e.plougoulm@groupeonepoint>
2021-03-23 16:01:38 +0100

    [MONGO_ATLAS] use correct mongo_uri

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = (handler) => async (req, res) => {
-  // Use new db connection
   await mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useFindAndModify: false,
