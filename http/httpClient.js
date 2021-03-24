8ab1f4945baba17387e26c38be02e44f52f143ea
e.plougoulm <e.plougoulm@groupeonepoint>
2021-03-24 11:35:59 +0100

    [baseURl] change for production

--- http/httpClient.js
+++ http/httpClient.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 const httpClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "http://p4-collectif.vercel.app"
+      : "http://localhost:3000",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
